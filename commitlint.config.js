ae7ecd8c665e649e34abd01dbbdb399713e5737f
Stefan@Lap <stefan.muennich@unibas.ch>
2021-04-30 22:19:16 +0200

    build(deps): configure commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,24 +1,29 @@
 module.exports = {
     extends: ['@commitlint/config-angular'],
     rules: {
-        'type-case': [2, 'always', ['lower-case', 'upper-case']],
-        'type-enum': [
+        'scope-case': [2, 'always', ['lower-case', 'upper-case']],
+        'scope-empty': [2, 'never'],
+        'scope-enum': [
             2,
             'always',
             [
-                'ANGULAR2',
-                '_DOCUMENTS',
-                'ONTOLOGIES',
-                'build',
-                'ci',
-                'docs',
-                'feat',
-                'fix',
-                'perf',
-                'refactor',
-                'revert',
-                'style',
-                'test'
+                'app',
+                'deps',
+                'core',
+                'shared',
+                'views',
+                'project',
+                'edition',
+                'webern',
+                'works',
+                'research',
+                'contact',
+                'page-not-found',
+                'testing',
+                'README',
+                'CHANGELOG',
+                'LICENSE',
+                'gh-actions'
             ]
         ]
     }
